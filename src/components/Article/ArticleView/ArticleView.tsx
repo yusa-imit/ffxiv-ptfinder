099793385d85e6ee81f054543ace7f13104dd5ee
yusa-imit <esimileiar81@gmail.com>
2022-08-26 23:28:52 +0900

    i18n changes

--- src/components/Article/ArticleView/ArticleView.tsx
+++ src/components/Article/ArticleView/ArticleView.tsx
@@ -42,15 +42,15 @@ export default function ArticleView({ article }: ArticleViewProps) {
     >
       <article style={{ width: '100%', position: 'relative' }}>
         <Stack>
-          <Section title={t('article_view_section_title_title')}>
+          <Section title={t('section_title_title')}>
             <Title order={1} style={{ wordBreak: 'break-word', lineBreak: 'auto' }}>
               {article.title}
             </Title>
           </Section>
-          <Section title={t('article_view_section_defaults_title')}>
+          <Section title={t('section_default_title')}>
             {article.isTemporary && (
               <Group>
-                <ArticleBadge>{t('article_view_isTemporary_badge')}</ArticleBadge>
+                <ArticleBadge>{t('section_default_isTemporary_badge')}</ArticleBadge>
               </Group>
             )}
             {
@@ -60,14 +60,16 @@ export default function ArticleView({ article }: ArticleViewProps) {
               <ArticleBadge>DEV_Unending Coil of Bahamut (Extreme)</ArticleBadge>
             </Group>
             <Group>
-              <ArticleBadge>{`${t('article_view_region')} : ${article.region}`}</ArticleBadge>
-              <ArticleBadge>{`${t('article_view_language')} : ${article.language}`}</ArticleBadge>
+              <ArticleBadge>{`${t('section_default_region')} : ${article.region}`}</ArticleBadge>
+              <ArticleBadge>{`${t('section_default_language')} : ${
+                article.language
+              }`}</ArticleBadge>
             </Group>
           </Section>
-          <Section title={t('article_view_section_jobs_title')}>
-            <SubTitle>{t('article_view_section_jobs_job_all')}</SubTitle>
+          <Section title={t('section_jobs_title')}>
+            <SubTitle>{t('section_jobs_job_all')}</SubTitle>
             {article.jobs.flat(3).length === 0 ? (
-              <Text>{t('article_view_section_jobs_job_not_specified')} </Text>
+              <Text>{t('section_jobs_job_not_specified')} </Text>
             ) : (
               <Group>
                 {Array.from(new Set(article.jobs.flat(3)), (v) => v)
@@ -77,7 +79,7 @@ export default function ArticleView({ article }: ArticleViewProps) {
                   ))}
               </Group>
             )}
-            <SubTitle>{t('article_view_section_jobs_job_position')}</SubTitle>
+            <SubTitle>{t('section_jobs_job_position')}</SubTitle>
             <Stack>
               {article.jobs.map((party, partyIndex) => (
                 <Group key={partyIndex}>
@@ -85,7 +87,7 @@ export default function ArticleView({ article }: ArticleViewProps) {
                     <Tooltip
                       label={
                         position.length === 0
-                          ? t('article_view_section_jobs_job_not_specified')
+                          ? t('section_jobs_job_not_specified')
                           : position.map((v) => t(`jobs_${v}`)).join(', ')
                       }
                       key={positionIndex}
@@ -99,8 +101,14 @@ export default function ArticleView({ article }: ArticleViewProps) {
               ))}
             </Stack>
           </Section>
-          <Section title={t('section_details_title')}></Section>
-          <Section title={t('section_schedules_title')}></Section>
+          <Section title={t('section_details_title')}>
+            <SubTitle>{t('section_details_essentials')}</SubTitle>
+            <SubTitle>{t('section_details_additionals')}</SubTitle>
+            <SubTitle>{t('section_details_language_restrictions')}</SubTitle>
+          </Section>
+          <Section title={t('section_schedules_title')}>
+            <SubTitle>{t('section_schedules_detail')}</SubTitle>
+          </Section>
           <Section title={t('section_description_title')}>
             {article.description.replace(/(<([^>]+)>)/gi, '') === '' ? (
               <Text>{t('section_description_no_description_provided')}</Text>
