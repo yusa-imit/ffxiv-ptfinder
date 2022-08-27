633d77b5e9973c8dcc2e30c75ccc7a2b09de3690
yusa-imit <esimileiar81@gmail.com>
2022-08-27 20:12:15 +0900

    optimizing

--- src/components/Article/Maker/Phase/Phase4/Phase4.tsx
+++ src/components/Article/Maker/Phase/Phase4/Phase4.tsx
@@ -27,10 +27,6 @@ export default function Phase4({ render, errorMessages, errorMessageHandler }: P
   const [desc, setDesc] = useState('');
   const [article, changeArticle] = useRecoilState(Article);
   const [edit, setEdit] = useState(false);
-  const [isDynamic, setIsDynamic] = useState(false);
-  const ref = useRef<Editor>();
-  const phase4Error = {};
-  const SelectData = {};
   const changeDescription = () => {
     changeArticle((prev) => {
       const newState = { ...prev };
@@ -48,69 +44,65 @@ export default function Phase4({ render, errorMessages, errorMessageHandler }: P
       clearTimeout(timer);
     };
   }, [desc]);
-
   useEffect(() => {
-    console.log('calling');
     const quill = document.getElementsByClassName('ql-editor')[0];
     if (quill) quill.setAttribute('style', 'min-height: 40vh');
   });
   return (
-    <RTELoadingContext.Provider value={setIsDynamic}>
-      <BigContainer
-        style={{ height: render ? 'fit-content' : 0 }}
-        className={classes.inner}
-        //style={{ position: current !== 0 ? 'absolute' : 'relative' }}
-      >
-        <PhaseStack title={t('phase4_description')}>
-          <RTEDynamic
-            value={article.description === desc ? article.description : desc}
-            onChange={setDesc}
-            controls={[
-              ['bold', 'strike', 'italic', 'underline'],
-              ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
-              ['clean'],
-              ['unorderedList', 'orderedList'],
-              ['link', 'blockquote', 'code', 'codeBlock'],
-              ['alignLeft', 'alignCenter', 'alignRight'],
-              ['sup', 'sub'],
-            ]}
-            style={{ width: '100%' }}
-          />
+    <BigContainer
+      style={{ height: render ? 'fit-content' : 0 }}
+      className={classes.inner}
+      //style={{ position: current !== 0 ? 'absolute' : 'relative' }}
+    >
+      <PhaseStack title={t('phase4_description')}>
+        <RTEDynamic
+          value={article.description === desc ? article.description : desc}
+          onChange={setDesc}
+          controls={[
+            ['bold', 'strike', 'italic', 'underline'],
+            ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+            ['clean'],
+            ['unorderedList', 'orderedList'],
+            ['link', 'blockquote', 'code', 'codeBlock'],
+            ['alignLeft', 'alignCenter', 'alignRight'],
+            ['sup', 'sub'],
+          ]}
+          style={{ width: '100%' }}
+        />
 
-          <Group position="right" style={{ width: '100%', position: 'relative' }}>
-            <Group
-              spacing="sm"
-              style={{
-                position: 'absolute',
-                left: 0,
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              <Text>
-                {edit ? (
-                  <Loader variant="dots" size="sm" />
-                ) : (
-                  <ThemeIcon
-                    radius="xl"
-                    size="sm"
-                    style={{
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}
-                  >
-                    <Check strokeWidth={4} />
-                  </ThemeIcon>
-                )}
-              </Text>
-              <Text>{edit ? t('phase4_description_editing') : t('phase4_description_saved')}</Text>
-            </Group>
-            <Button onClick={changeDescription}>{t('phase4_save_button')}</Button>
+        <Group position="right" style={{ width: '100%', position: 'relative' }}>
+          <Group
+            spacing="sm"
+            style={{
+              position: 'absolute',
+              left: 0,
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Text>
+              {edit ? (
+                <Loader variant="dots" size="sm" />
+              ) : (
+                <ThemeIcon
+                  radius="xl"
+                  size="sm"
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
+                  <Check strokeWidth={4} />
+                </ThemeIcon>
+              )}
+            </Text>
+            <Text>{edit ? t('phase4_description_editing') : t('phase4_description_saved')}</Text>
           </Group>
-        </PhaseStack>
-      </BigContainer>
-    </RTELoadingContext.Provider>
+          <Button onClick={changeDescription}>{t('phase4_save_button')}</Button>
+        </Group>
+      </PhaseStack>
+    </BigContainer>
   );
 }
--- src/components/RTEDynamic.tsx
+++ src/components/RTEDynamic.tsx
@@ -4,19 +4,4 @@ import { RTELoadingContext } from './Article/RTELoadingContext';
 
 export default dynamic(() => import('@mantine/rte'), {
   ssr: false,
-  loading: () => {
-    const setLoading = useContext(RTELoadingContext);
-    useEffect(() => {
-      if (setLoading) {
-        console.log('loading-start');
-        setLoading(true);
-        return () => {
-          console.log('loading-end');
-          setLoading(false);
-        };
-      }
-      return () => {};
-    }, [setLoading]);
-    return null;
-  },
 });
