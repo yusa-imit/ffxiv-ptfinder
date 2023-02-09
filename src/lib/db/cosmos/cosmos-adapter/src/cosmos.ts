c85b32ea1fb0cab8635fd0b4186962f19186301c
yusa-imit <esimileiar81@gmail.com>
2023-02-09 20:16:53 +0900

    #7 finish auth adapter with cosmos db

---
+++ src/lib/db/cosmos/cosmos-adapter/src/cosmos.ts
@@ -0,0 +1,128 @@
+import {
+  Container,
+  ContainerRequest,
+  CosmosClient,
+  CosmosClientOptions,
+  Database,
+  DatabaseRequest,
+  RequestOptions,
+} from '@azure/cosmos';
+
+let client: null | CosmosClient = null;
+let db: Database | null = null;
+const container: Record<string, Container> = {};
+
+const DEFAULT_DB_NAME = 'next-auth' as const;
+const CONTAINER_USER = 'users' as const;
+const CONTAINER_ACCOUNTS = 'accounts' as const;
+const CONTAINER_SESSIONS = 'sessions' as const;
+const CONTAINER_TOKENS = 'tokens' as const;
+
+function setClient(options: CosmosClientOptions) {
+  client = new CosmosClient(options);
+}
+function getClient(options?: CosmosClientOptions): CosmosClient {
+  if (client === null) {
+    if (!options?.endpoint || !options.key) {
+      throw new Error('DB initialize failed');
+    }
+    setClient(options);
+  }
+  return client as CosmosClient;
+}
+
+async function setDb(clientOptions: CosmosClientOptions, dbOptions: CosmosDataBaseOptions) {
+  db = (
+    await getClient(clientOptions).databases.createIfNotExists(dbOptions.body, dbOptions.options)
+  ).database;
+}
+
+interface CosmosDBBaseOptions {
+  clientOptions: CosmosClientOptions;
+  dbOptions?: CosmosDataBaseOptions;
+}
+interface CosmosDataBaseOptions {
+  body: DatabaseRequest;
+  options?: RequestOptions;
+}
+interface CosmosContainerOptions {
+  body: ContainerRequest;
+  options?: RequestOptions;
+}
+
+export interface CosmosDBInitOptions extends CosmosDBBaseOptions {
+  containerOptions?: {
+    usersOptions?: CosmosContainerOptions;
+    accountsOptions?: CosmosContainerOptions;
+    sessionsOptions?: CosmosContainerOptions;
+    tokensOptions?: CosmosContainerOptions;
+  };
+}
+
+async function init(options: CosmosDBBaseOptions) {
+  const { clientOptions, dbOptions } = options;
+  if (client === null) setClient(clientOptions);
+  if (db === null) {
+    await setDb(clientOptions, dbOptions ?? { body: { id: DEFAULT_DB_NAME } });
+  }
+}
+
+export async function getContainer(
+  dbOptions: CosmosDBBaseOptions,
+  body: ContainerRequest & { id: string },
+  options?: RequestOptions
+) {
+  if (!container[body.id]) {
+    await init(dbOptions);
+    container[body.id] = (
+      await (db as Database).containers.createIfNotExists(body, options)
+    ).container;
+  }
+  return container[body.id] as Container;
+}
+export const getCosmos = (options: CosmosDBInitOptions) => {
+  const { containerOptions, ...baseOptions } = options;
+  return {
+    users: async () => {
+      return getContainer(
+        baseOptions,
+        {
+          id: CONTAINER_USER,
+          uniqueKeyPolicy: { uniqueKeys: [{ paths: ['/email'] }] },
+          ...containerOptions?.usersOptions?.body,
+        },
+        containerOptions?.usersOptions?.options
+      );
+    },
+    accounts: async () => {
+      return getContainer(
+        baseOptions,
+        {
+          id: CONTAINER_ACCOUNTS,
+          ...containerOptions?.accountsOptions?.body,
+        },
+        containerOptions?.accountsOptions?.options
+      );
+    },
+    sessions: async () => {
+      return getContainer(
+        baseOptions,
+        {
+          id: CONTAINER_SESSIONS,
+          ...containerOptions?.sessionsOptions?.body,
+        },
+        containerOptions?.sessionsOptions?.options
+      );
+    },
+    tokens: async () => {
+      return getContainer(
+        baseOptions,
+        {
+          id: CONTAINER_TOKENS,
+          ...containerOptions?.tokensOptions?.body,
+        },
+        containerOptions?.tokensOptions?.options
+      );
+    },
+  };
+};
---
+++ src/lib/db/cosmos/cosmos-adapter/test/adapter.test.ts
@@ -0,0 +1,82 @@
+import https from 'node:https';
+import { SqlQuerySpec } from '@azure/cosmos';
+import { runBasicTests } from './adapterTest';
+import { CosmosAdapter } from '../src';
+import { getCosmos } from '../src/cosmos';
+import { convertCosmosDocument } from '../src/util';
+
+const ENDPOINT = 'https://localhost:8081';
+const KEY =
+  'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==';
+
+const UnrejectfulAgent = new https.Agent({ rejectUnauthorized: false });
+
+const CLIENT_OPTIONS = {
+  endpoint: ENDPOINT,
+  key: KEY,
+  agent: UnrejectfulAgent,
+};
+
+const db = getCosmos({ clientOptions: CLIENT_OPTIONS });
+
+runBasicTests({
+  adapter: CosmosAdapter({
+    clientOptions: CLIENT_OPTIONS,
+  }),
+  db: {
+    async disconnect() {
+      await (await db.accounts()).delete();
+      await (await db.sessions()).delete();
+      await (await db.tokens()).delete();
+      await (await db.users()).delete();
+    },
+    async session(sessionToken) {
+      const snapshotQuery: SqlQuerySpec = {
+        query: `select * from sessions s where s.sessionToken=@sessionTokenValue`,
+        parameters: [{ name: '@sessionTokenValue', value: sessionToken }],
+      };
+      const { resources } = await (await db.sessions()).items.query(snapshotQuery).fetchAll();
+
+      if (resources.length > 0) {
+        return convertCosmosDocument(resources[0]);
+      }
+      return null;
+    },
+    async user(id) {
+      const { resource } = await (await db.users()).item(id).read();
+      if (resource) return convertCosmosDocument(resource);
+      return null;
+    },
+    async account({ provider, providerAccountId }) {
+      const snapshotQuery: SqlQuerySpec = {
+        query:
+          'select * from accounts a where a.provider=@providerValue and a.providerAccountId=@providerAccountIdValue',
+        parameters: [
+          { name: '@providerValue', value: provider },
+          { name: '@providerAccountIdValue', value: providerAccountId },
+        ],
+      };
+      const { resources } = await (await db.accounts()).items.query(snapshotQuery).fetchAll();
+
+      if (resources.length > 0) {
+        return convertCosmosDocument(resources[0]);
+      }
+      return null;
+    },
+    async verificationToken({ identifier, token }) {
+      const snapshotQuery: SqlQuerySpec = {
+        query: 'select * from tokens v where v.identifier=@identifierValue and v.token=@tokenValue',
+        parameters: [
+          { name: '@identifierValue', value: identifier },
+          { name: '@tokenValue', value: token },
+        ],
+      };
+      const { resources } = await (await db.tokens()).items.query(snapshotQuery).fetchAll();
+
+      if (resources.length > 0) {
+        return convertCosmosDocument(resources[0]);
+      }
+      throw new Error('No exact matching token.');
+    },
+  },
+});
